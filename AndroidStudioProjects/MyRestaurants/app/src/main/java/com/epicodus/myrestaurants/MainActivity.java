37b3880dc1f4fc5c93a7994767b9dd35b38c524c
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-17 17:16:11 -0800

    add list view and include butterknife

--- AndroidStudioProjects/MyRestaurants/app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ AndroidStudioProjects/MyRestaurants/app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,26 +1,33 @@
 package com.epicodus.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        //injection
+        ButterKnife.bind(this);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
 
 
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 intent.putExtra("location", location);
                 startActivity(intent);
+
+
+
+
 //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
 //A toast is a simple pop up message...three parameters; a context, a message, and a duration
             }
--- AndroidStudioProjects/MyRestaurants/app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ AndroidStudioProjects/MyRestaurants/app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -2,21 +2,58 @@ package com.epicodus.myrestaurants;
 
 import android.content.Intent;
 import android.support.v4.widget.TextViewCompat;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
-    private TextView mLocationTextView;
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+    @Bind(R.id.listView) ListView mListView;
 
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
+
+    //initialize
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        //find components with id
+        ButterKnife.bind(this);
+
+        //  we create a new ArrayAdapter providing three arguments:
+        // 1. this, which represents the current context,
+        // 2. android.R.layout.simple_list_item_1, which is a layout built into Android that provides standard appearance for text in a list,
+        //3. an ArrayList called restaurants (not seen here)
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+
+
+        //display a toast containing the restaurant name when a list item is clicked
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
+        String location = intent.getStringExtra("location");    //Retrieve extended data from the intent.
         mLocationTextView.setText("Here are all the restaurants near: " + location);
+
     }
 }
