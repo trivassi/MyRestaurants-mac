357428cac9762e3560938757989a0ce17860b88a
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-26 16:11:59 -0500

    add okhttp to app

---
+++ AndroidStudioProjects/MyRestaurants/app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -0,0 +1,12 @@
+package com.epicodus.myrestaurants;
+
+/**
+ * Created by T on 11/26/16.
+ */
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
